ff06d3e57dbf2d8b152acce9f07e854c81007326
nhasley <nova@lingmentum.com>
2019-11-19 16:10:48 -0600

    adding favorites

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import '../widgets/meal_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  FavoritesScreen(this.favoriteMeals);
+
   @override
   Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+
     return Center(
-      child: Text('The Favorites'),
+      child: Text('You have no favorites yet - start adding some!'),
     );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, id) {
+          return MealItem(
+            id: favoriteMeals[id].id,
+            title: favoriteMeals[id].title,
+            imageUrl: favoriteMeals[id].imageUrl,
+            duration: favoriteMeals[id].duration,
+            complexity: favoriteMeals[id].complexity,
+            affordability: favoriteMeals[id].affordability,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
   }
 }
